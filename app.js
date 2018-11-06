7057059e7a0799d452025b2cfaeec90729e6d023
Alexandre Weber Dalabona <alexandre.dalabona@gmail.com>
2018-11-06 21:56:34 -0200

    estrutura do projeto e views

---
+++ app.js
@@ -0,0 +1,5 @@
+let app = require('./config/server.js');
+
+app.listen(80, () => {
+    console.log("Servidor no ar");
+});
---
+++ app/routes/chat.js
@@ -0,0 +1,9 @@
+module.exports = ((app) => {
+    app.post('/chat', ((req, res) => {
+        app.app.controllers.chat.chat(app, req, res);
+    }));
+
+    app.get('/chat', ((req, res) => {
+        app.app.controllers.chat.chat(app, req, res);
+    }));
+});
