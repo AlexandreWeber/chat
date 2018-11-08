75f9edc4149e22a69965b4b0068ff4f0169238ff
Alexandre Weber Dalabona <alexandre.dalabona@gmail.com>
2018-11-07 23:02:01 -0200

    Validação login e socket.io

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,13 @@
 module.exports.chat = ((app, req, res) => {
+    let apelido = req.body.apelido;
+
+    req.assert('apelido', 'Informe um apelido').notEmpty();
+    req.assert('apelido', 'O apelido deve possuir no mínimo 3 e no máximo 15 caracteres').len(3, 15);
+
+    if (req.validationErrors()) {
+        res.render('index', { erros: req.validationErrors()});
+        return;
+    }
+
     res.render('chat');
 });
