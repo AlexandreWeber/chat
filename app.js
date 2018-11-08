21edd0e35b9f6b466382288bc8245063713ef68e
Alexandre Weber Dalabona <alexandre.dalabona@gmail.com>
2018-11-07 23:45:11 -0200

    socket

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let app = require('./config/server.js');
 
-app.listen(80, () => {
+let server  = app.listen(80, () => {
     console.log("Servidor no ar");
 });
+
+let io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', ((socket) => {
+    console.log('conectado');
+
+    io.on('newUser', ((event) => {
+        console.log(event)
+    }));
+}));
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -5,9 +5,14 @@ module.exports.chat = ((app, req, res) => {
     req.assert('apelido', 'O apelido deve possuir no mínimo 3 e no máximo 15 caracteres').len(3, 15);
 
     if (req.validationErrors()) {
-        res.render('index', { erros: req.validationErrors()});
+        res.render('index', { erros: req.validationErrors(), usuario: apelido});
         return;
     }
+
+    app.get('io').emit('newUser', {
+        apelido: apelido,
+        msg: 'Acabou de entrar no chat'
+    });
 
     res.render('chat');
 });
